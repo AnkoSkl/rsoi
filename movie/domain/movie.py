c65aa9b907b114f0e88a18389945d1995ae7a8e6
Anna <anko136660422@gmail.com>
2018-10-20 23:54:31 +0300

    Добавлен класс Movie

---
+++ movie/domain/movie.py
@@ -0,0 +1,6 @@
+class Movie:
+    def __init__(self, movie_id, name, description, length):
+        self.id = movie_id
+        self.name = name
+        self.description = description
+        self.length = length
