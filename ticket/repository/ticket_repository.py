4cde8488f03cefcc2e29233644833affee91f84f
Anna <anko136660422@gmail.com>
2018-10-21 22:15:06 +0300

    Добавлен класс TicketRepository в ticket_repository

--- ticket/repository/ticket_repository.py
+++ ticket/repository/ticket_repository.py
@@ -9,3 +9,34 @@ db = MongoAlchemy(app)
 class Tickets(db.Document):
     seance_id = db.StringField()
     seat_number = db.IntField()
+
+
+class TicketRepository:
+    def create(self, seance_id, seat_number):
+        ticket = Tickets(seance_id=seance_id, seat_number=seat_number)
+        ticket.save()
+        return ticket.mongo_id
+
+    def get(self, ticket_id):
+        if self.exists(ticket_id):
+            ticket = Tickets.query.get(ticket_id)
+            return Ticket(ticket_id=ticket.mongo_id, seance_id=ticket.seance_id, seat_number=ticket.seat_number)
+        else:
+            return None
+
+    def read_all(self):
+        tickets = []
+        all_tickets = Tickets.query.all()
+        for ticket in all_tickets:
+            tickets.append(Ticket(ticket_id=ticket.mongo_id, seance_id=ticket.seance_id,
+                                  seat_number=ticket.seat_number))
+        return tickets
+
+    def delete(self, ticket_id):
+        if self.exists(ticket_id):
+            ticket = Tickets.query.get(ticket_id)
+            ticket.remove()
+
+    def exists(self, ticket_id):
+        result = Tickets.query.get(ticket_id)
+        return result is not None
