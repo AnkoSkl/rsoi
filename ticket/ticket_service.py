be287a28b974ef9718e81f627d447e86f7a886dd
Anna <anko136660422@gmail.com>
2018-10-21 22:29:51 +0300

    Создан модуль ticket_service

---
+++ ticket/ticket_service.py
@@ -0,0 +1,16 @@
+from ticket import app
+from flask_restful import Api
+from ticket.rest_api.ticket_resource import *
+from ticket.repository.ticket_repository import Tickets
+
+
+api = Api(app)
+service_namespace = "/tickets"
+
+api.add_resource(TicketListResource, "/tickets")
+api.add_resource(TicketResource, "/tickets/<ticket_id>")
+api.add_resource(TicketCreateResource, "/tickets/create")
+
+
+if __name__ == '__main__':
+    app.run(port=5002)
