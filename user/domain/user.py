77dd8d4682b34e7b3b6821322214b9023943d6b0
Anna <anko136660422@gmail.com>
2018-10-21 20:08:45 +0300

    Добавлен класс UserRepository в user_repository

--- user/domain/user.py
+++ user/domain/user.py
@@ -1,4 +1,5 @@
 class User:
-    def __init__(self, name, password):
+    def __init__(self, user_id, name, password):
+        self.id = user_id
         self.name = name
         self.password = password
--- user/repository/user_repository.py
+++ user/repository/user_repository.py
@@ -10,4 +10,34 @@ class Users(db.Document):
     name = db.StringField()
     password = db.StringField()
 
-    
+
+class UserRepository:
+    def create(self, name, password):
+        user = Users(name=name, password=password)
+        user.save()
+        return user.mongo_id
+
+    def get(self, user_id):
+        if self.exists(user_id):
+            user = Users.query.get(user_id)
+            return User(user_id=user.mongo_id, name=user.name, password=user.password)
+        else:
+            return None
+
+    def read_all(self):
+        users = []
+        all_users = Users.query.all()
+        for user in all_users:
+            users.append(User(user_id=user.mongo_id, name=user.name, password=user.password))
+        return users
+
+    def delete(self, user_id):
+        if self.exists(user_id):
+            user = Users.query.get(user_id)
+            user.remove()
+
+    def exists(self, user_id):
+        result = Users.query.get(user_id)
+        return result is not None
+
+
