a795cef34ccddc4bcf68e20c1f9ac00087023419
Anna <anko136660422@gmail.com>
2018-10-23 22:07:05 +0300

    Удален класс GatewayTicketListResource

--- gateway/rest_api/gateway_api.py
+++ gateway/rest_api/gateway_api.py
@@ -31,14 +31,3 @@ class GatewayTicketResource(Resource):
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
-
-
-class GatewayTicketListResource(Resource):
-    def get(self):
-        sess = requests.session()
-        for cookie in flask.request.cookies:
-            sess.cookies[cookie] = flask.request.cookies[cookie]
-        response = requests.get("http://127.0.0.1:5003/tickets")
-        result = flask.Response(status=response.status_code, headers=response.headers.items(),
-                                response=response.content)
-        return result
