d9b65d6d183414459dd9989a9d179d3a927e0725
Anna <anko136660422@gmail.com>
2018-10-23 13:12:55 +0300

    Создан класс GatewayTicketResource в gateway_api

---
+++ gateway/rest_api/gateway_api.py
@@ -0,0 +1,33 @@
+from flask_restful import Resource, reqparse
+import flask
+import jsonpickle
+import requests
+
+
+class GatewayTicketResource(Resource):
+    def get(self, ticket_id):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = requests.get("/tickets/%s" % ticket_id)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
+
+    def post(self):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = sess.post("http://127.0.0.1:5003/tickets", data=flask.request.data)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
+
+    def delete(self, ticket_id):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = sess.delete("http://127.0.0.1:5003/tickets/%s" % ticket_id)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
