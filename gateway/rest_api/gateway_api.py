f98db0e59a596f35ed93d362b7d6febe650fc4d5
Anna <anko136660422@gmail.com>
2018-10-23 23:22:04 +0300

    Добавлен класс GatewayUserResource

--- gateway/rest_api/gateway_api.py
+++ gateway/rest_api/gateway_api.py
@@ -81,3 +81,60 @@ class GatewaySeanceListResource(Resource):
                                 response=response.content)
         return result
 
+
+class GatewayMovieResource(Resource):
+    def get(self, movie_id):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = requests.get("http://127.0.0.1:5001/movies/%s" % movie_id)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
+
+    def post(self):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = sess.post("http://127.0.0.1:5001/movies/create", data=flask.request.data)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
+
+    def delete(self, movie_id):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = sess.delete("http://127.0.0.1:5001/movies/%s" % movie_id)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
+
+
+class GatewayUserResource(Resource):
+    def get(self, user_id):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = requests.get("http://127.0.0.1:5004/users/%s" % user_id)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
+
+    def post(self):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = sess.post("http://127.0.0.1:5004/users/create", data=flask.request.data)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
+
+    def delete(self, user_id):
+        sess = requests.session()
+        for cookie in flask.request.cookies:
+            sess.cookies[cookie] = flask.request.cookies[cookie]
+        response = sess.delete("http://127.0.0.1:5004/users/%s" % user_id)
+        result = flask.Response(status=response.status_code, headers=response.headers.items(),
+                                response=response.content)
+        return result
