48bad1cc90ee94ff2800bbd84b5d5e10e6a101e9
Anna <anko136660422@gmail.com>
2018-10-27 19:14:43 +0300

    Информация о сеансе дополнена информацией о фильме

--- gateway/rest_api/gateway_api.py
+++ gateway/rest_api/gateway_api.py
@@ -3,19 +3,18 @@ import flask
 import jsonpickle
 import requests
 from ticket.domain.ticket import Ticket
+from seance.domain.seance import Seance
 
 
 class GatewayTicketResource(Resource):
     def get(self, ticket_id):
-        sess = requests.session()
         response = requests.get("http://127.0.0.1:5003/tickets/%s" % ticket_id)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
 
     def delete(self, ticket_id):
-        sess = requests.session()
-        response = sess.delete("http://127.0.0.1:5003/tickets/%s" % ticket_id)
+        response = requests.delete("http://127.0.0.1:5003/tickets/%s" % ticket_id)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -23,8 +22,7 @@ class GatewayTicketResource(Resource):
 
 class GatewayTicketCreateResource(Resource):
     def post(self):
-        sess = requests.session()
-        response = sess.post("http://127.0.0.1:5003/tickets/create", data=flask.request.data)
+        response = requests.post("http://127.0.0.1:5003/tickets/create", data=flask.request.data)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -32,22 +30,26 @@ class GatewayTicketCreateResource(Resource):
 
 class GatewaySeanceResource(Resource):
     def get(self, seance_id):
-        sess = requests.session()
-        response = requests.get("http://127.0.0.1:5002/seances/%s" % seance_id)
-        result = flask.Response(status=response.status_code, headers=response.headers.items(),
-                                response=response.content)
+        response_seance = requests.get("http://127.0.0.1:5002/seances/%s" % seance_id)
+
+        seance = jsonpickle.decode(response_seance.content)
+        movie_id = str(seance.movie_id)
+
+        response_movie = requests.get("http://127.0.0.1:5001/movies/%s" % movie_id)
+        movie = jsonpickle.decode(response_movie.content)
+        response = {"seance":seance, "movie":movie}
+        result = flask.Response(status=response_seance.status_code, headers=response_seance.headers.items(),
+                                response=jsonpickle.encode(response))
         return result
 
     def patch(self, seance_id):
-        sess = requests.session()
-        response = sess.patch("http://127.0.0.1:5002/seances/%s" % seance_id, data=flask.request.data)
+        response = requests.patch("http://127.0.0.1:5002/seances/%s" % seance_id, data=flask.request.data)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
 
     def delete(self, seance_id):
-        sess = requests.session()
-        response = sess.delete("http://127.0.0.1:5002/seances/%s" % seance_id)
+        response = requests.delete("http://127.0.0.1:5002/seances/%s" % seance_id)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -55,8 +57,7 @@ class GatewaySeanceResource(Resource):
 
 class GatewaySeanceCreateResource(Resource):
     def post(self):
-        sess = requests.session()
-        response = sess.post("http://127.0.0.1:5002/seances/create", data=flask.request.data)
+        response = requests.post("http://127.0.0.1:5002/seances/create", data=flask.request.data)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -64,8 +65,7 @@ class GatewaySeanceCreateResource(Resource):
 
 class GatewaySeanceListResource(Resource):
     def get(self):
-        sess = requests.session()
-        response = sess.get("http://127.0.0.1:5002/seances")
+        response = requests.get("http://127.0.0.1:5002/seances")
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -73,15 +73,13 @@ class GatewaySeanceListResource(Resource):
 
 class GatewayMovieResource(Resource):
     def get(self, movie_id):
-        sess = requests.session()
         response = requests.get("http://127.0.0.1:5001/movies/%s" % movie_id)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
 
     def delete(self, movie_id):
-        sess = requests.session()
-        response = sess.delete("http://127.0.0.1:5001/movies/%s" % movie_id)
+        response = requests.delete("http://127.0.0.1:5001/movies/%s" % movie_id)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -89,8 +87,7 @@ class GatewayMovieResource(Resource):
 
 class GatewayMovieCreateResource(Resource):
     def post(self):
-        sess = requests.session()
-        response = sess.post("http://127.0.0.1:5001/movies/create", data=flask.request.data)
+        response = requests.post("http://127.0.0.1:5001/movies/create", data=flask.request.data)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -98,15 +95,13 @@ class GatewayMovieCreateResource(Resource):
 
 class GatewayUserResource(Resource):
     def get(self, user_id):
-        sess = requests.session()
         response = requests.get("http://127.0.0.1:5004/users/%s" % user_id)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
 
     def delete(self, user_id):
-        sess = requests.session()
-        response = sess.delete("http://127.0.0.1:5004/users/%s" % user_id)
+        response = requests.delete("http://127.0.0.1:5004/users/%s" % user_id)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -114,8 +109,7 @@ class GatewayUserResource(Resource):
 
 class GatewayUserCreateResource(Resource):
     def post(self):
-        sess = requests.session()
-        response = sess.post("http://127.0.0.1:5004/users/create", data=flask.request.data)
+        response = requests.post("http://127.0.0.1:5004/users/create", data=flask.request.data)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
@@ -126,21 +120,20 @@ class GatewayBuyTicket(Resource):
 
     def post(self):
         payload = jsonpickle.decode(flask.request.data)
-        sess = requests.session()
         payload1 = {'seat_number': payload["seat_number"], 'status': 'buy'}
-        response = sess.patch("http://127.0.0.1:5002/seances/%s" % payload["seance_id"], jsonpickle.encode(payload1))
+        response = requests.patch("http://127.0.0.1:5002/seances/%s" % payload["seance_id"], jsonpickle.encode(payload1))
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         if result.status_code != 201:
             return result
 
-        response = sess.post("http://127.0.0.1:5003/tickets/create", jsonpickle.encode(payload))
+        response = requests.post("http://127.0.0.1:5003/tickets/create", jsonpickle.encode(payload))
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
 
         ticket = jsonpickle.decode(response.content)
         payload3 = {'ticket_id': str(ticket.id), 'status': 'buy'}
-        sess.patch("http://127.0.0.1:5004/users/%s" % self.user_id, jsonpickle.encode(payload3))
+        requests.patch("http://127.0.0.1:5004/users/%s" % self.user_id, jsonpickle.encode(payload3))
         return result
 
 
@@ -148,17 +141,16 @@ class GatewayReturnTicket(Resource):
     user_id = "5bd0a351af13c713737dae92"
 
     def post(self, ticket_id):
-        sess = requests.session()
-        response = sess.get("http://127.0.0.1:5003/tickets/%s" % ticket_id)
+        response = requests.get("http://127.0.0.1:5003/tickets/%s" % ticket_id)
 
         ticket = jsonpickle.decode(response.content)
         payload1 = {'seat_number': ticket.seat_number, 'status': 'release'}
-        sess.patch("http://127.0.0.1:5002/seances/%s" % ticket.seance_id, jsonpickle.encode(payload1))
+        requests.patch("http://127.0.0.1:5002/seances/%s" % ticket.seance_id, jsonpickle.encode(payload1))
 
         payload3 = {'ticket_id': ticket_id, 'status': 'release'}
-        sess.patch("http://127.0.0.1:5004/users/%s" % self.user_id, jsonpickle.encode(payload3))
+        requests.patch("http://127.0.0.1:5004/users/%s" % self.user_id, jsonpickle.encode(payload3))
 
-        response = sess.delete("http://127.0.0.1:5003/tickets/%s" % ticket_id)
+        response = requests.delete("http://127.0.0.1:5003/tickets/%s" % ticket_id)
         result = flask.Response(status=response.status_code, headers=response.headers.items(),
                                 response=response.content)
         return result
