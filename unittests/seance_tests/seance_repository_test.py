2898526abe9676fd316c46346aaf1f04a086a924
Anna <anko136660422@gmail.com>
2018-10-31 10:28:42 +0300

    Добавлены модульные тесты для movie_repository

---
+++ unittests/seance_tests/seance_repository_test.py
@@ -0,0 +1,86 @@
+import unittest
+from seance.repository.seance_repository import SeanceRepository
+from seance.domain.seance import Seance
+from flask_mongoalchemy import fields
+
+
+class TestSeanceRepository(unittest.TestCase):
+    def test_create(self):
+        rep = SeanceRepository()
+        id1 = rep.create('5bd89b59af13c757e1b7f3fd', '01.01.2018_12:00', 50)
+        id2 = rep.create('5bd89b59af13c757e1b7f3fd', '01.01.2018_12:00', 50)
+        self.assertNotEqual(id1, id2)
+        rep.delete(id1)
+        rep.delete(id2)
+
+    def test_get_exists(self):
+        rep = SeanceRepository()
+        seance1 = rep.get('5bd897f8af13c78fe908cb98')
+        seance2 = Seance(seance_id=fields.ObjectId('5bd897f8af13c78fe908cb98'),
+                         movie_id=fields.ObjectId('5bd0a513af13c7251f913dd9'), date_time='22.11.2018_10:15',
+                         seats=[False, True, False, False, False, True, True, True, True, True, True, True, True, True,
+                                True, True, True, True, True, True, True, True, True, True, True, True, True, True,
+                                True, True, True, True, True, True, True, True, True, True, True, True, True, True,
+                                True, True, True, True, True, True, True, True, True, True, True, True, True, True,
+                                True, True, True, True, True, True, True, True, True, True, True, True, True, True,
+                                True, True, True, True, True, True, True, True, True, True, True, True, True, True,
+                                True, True, True, True, True, True, True, True, True, True, True, True, True, True,
+                                True, True])
+        self.assertEqual(seance1, seance2)
+
+    def test_get_false(self):
+        rep = SeanceRepository()
+        seance = rep.get('5bd89b59af1')
+        self.assertIsNone(seance)
+
+    def test_read_paginated(self):
+        rep = SeanceRepository()
+        seances = rep.read_paginated(1, 5)
+        self.assertLessEqual(len(seances), 5)
+
+    def test_delete(self):
+        rep = SeanceRepository()
+        seance_id = rep.create('5bd89b59af13c757e1b7f3fd', '01.01.2018_12:00', 50)
+        rep.delete(seance_id)
+        self.assertFalse(rep.exists(seance_id))
+
+    def test_get_a_seat_true(self):
+        rep = SeanceRepository()
+        boolean = rep.get_a_seat('5bd897f8af13c78fe908cb98', 2)
+        self.assertTrue(boolean)
+        rep.free_a_seat('5bd897f8af13c78fe908cb98', 2)
+
+    def test_get_a_seat_false(self):
+        rep = SeanceRepository()
+        boolean = rep.get_a_seat('5bd897f8af13c78fe908cb98', 1)
+        self.assertFalse(boolean)
+
+    def test_get_a_seat_none(self):
+        rep = SeanceRepository()
+        boolean = rep.get_a_seat('5bd897f8af', 1)
+        self.assertIsNone(boolean)
+
+    def test_free_a_seat_true(self):
+        rep = SeanceRepository()
+        boolean = rep.free_a_seat('5bd897f8af13c78fe908cb98', 1)
+        self.assertTrue(boolean)
+        rep.get_a_seat('5bd897f8af13c78fe908cb98', 1)
+
+    def test_free_a_seat_false(self):
+        rep = SeanceRepository()
+        boolean = rep.free_a_seat('5bd897f8af13c78fe908cb98', 2)
+        self.assertFalse(boolean)
+
+    def test_free_a_seat_none(self):
+        rep = SeanceRepository()
+        boolean = rep.free_a_seat('5bd897f8af', 2)
+        self.assertIsNone(boolean)
+
+    def test_exists(self):
+        rep = SeanceRepository()
+        boolean = rep.exists('5bd897f8af13c78fe908cb98')
+        self.assertTrue(boolean)
+
+
+if __name__ == '__main__':
+    unittest.main()
